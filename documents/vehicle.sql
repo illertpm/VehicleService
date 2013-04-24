6e7fbb1491f81c5cd1d8424a3f8eb82a3a29b2b1
Patrick Northcutt <pnorthcutt@charter.net>
2013-04-24 13:44:03 -0500

    update sql file

--- documents/vehicle.sql
+++ documents/vehicle.sql
@@ -1,10 +1,11 @@
 DROP DATABASE IF EXISTS VehicleDB;
 CREATE DATABASE VehicleDB;
 USE VehicleDB;
-CREATE TABLE Vehicle (year INT NOT NULL,
-   make VARCHAR(100)  PRIMARY KEY NOT NULL,
+CREATE TABLE Vehicle (vehicleId INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+   year INT NOT NULL,
+   make VARCHAR(100)   NOT NULL,
    model  VARCHAR(100)  NOT NULL
    );
-INSERT INTO Vehicle {year,make,model} values {1990,"KIA","Sorento"};
-INSERT INTO Vehicle {year,make,model} values {1992,"Ford","F150"};
-INSERT INTO Vehicle {year,make,model} values {2000,"Ford","Mustang"};
+INSERT INTO Vehicle (year,make,model) VALUES (1990,"KIA","Sorento");
+INSERT INTO Vehicle (year,make,model) values (1992,"Ford","F150");
+INSERT INTO Vehicle (year,make,model) values (2000,"Ford","Mustang");
---
+++ src/main/java/com/northcutt/vehicle/beans/VehicleBean.java
@@ -0,0 +1,5 @@
+package com.northcutt.vehicle.beans;
+
+public class VehicleBean {
+
+}
